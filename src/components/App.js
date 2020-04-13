7fd25546df5694677a7ca2dc7a6d03c0d0e355c2
Takahiro <tkhr_621@docomo.ne.jp>
2020-04-13 17:09:50 +0900

    Fix container-fluid

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,11 @@ export const App = () => {
   const [state, dispatch] = useReducer(reducer, [])
   
   return (
-    <>
+    <div className="container-fluid">
       <h1>todoアプリ</h1>
       <TodoForm state={state} dispatch={dispatch} />
       <hr />
       <Todos state={state} dispatch={dispatch} />
-    </>
+    </div>
   )
 } 
