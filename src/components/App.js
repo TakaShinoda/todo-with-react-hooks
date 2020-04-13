d26017097712cac81084a74f42c2670464776d4e
Takahiro <tkhr_621@docomo.ne.jp>
2020-04-13 15:38:35 +0900

    入力されたタスクを表示

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { TodoForm } from './TodoForm'
-import { Todos } from './Todos'
+//import { Todos } from './Todos'
 
 export const App = () => {
   return (
     <>
       <h1>todoアプリ</h1>
       <TodoForm />
-      <hr />
-      <Todos />
+      {/*<hr />*/}
     </>
   )
 } 
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
-export const TodoList = () => {
-    
+export const TodoList = ({ dispatch, task }) => {
+    const id = task.id
+    return (
+        <tr>
+            <td>{id}</td>
+            <td>{task.title}</td>
+        </tr>
+    )
+
 }
