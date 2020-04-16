69f25f0b17ab7bcb15cd1271407a2ce79deff794
Takahiro <tkhr_621@docomo.ne.jp>
2020-04-16 11:32:57 +0900

    Fix

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -14,7 +14,7 @@ export const Todos = ({ state, dispatch }) => {
                 check
             })
         }
-        setCheck(false)
+        //setCheck(false)
     }
 
     const deleteAllTasks = e => {
@@ -26,7 +26,7 @@ export const Todos = ({ state, dispatch }) => {
     return (
         <>
             <h2>タスク一覧</h2>
-            {/*<button className="btn btn-danger" onClick={deleteTask}>削除</button>*/}
+            <button className="btn btn-danger" onClick={deleteTask}>削除</button>
             <button className="btn btn-danger" onClick={deleteAllTasks}>全て削除</button>
             <table className="table">
                 <thead className="thead-light">
